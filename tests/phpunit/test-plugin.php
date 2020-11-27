cd6d791fbab638bcfa50d5d337aeeed125b52dc0
John Blackbourn <johnbillion@gmail.com>
2020-11-27 16:02:53 +0100

    Add an initial test for the readme.

---
+++ tests/phpunit/test-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Meta tests for the plugin.
+ *
+ * @package authorship
+ */
+
+declare( strict_types=1 );
+
+namespace Authorship\Tests;
+
+class TestPlugin extends TestCase {
+	public function testReadmeIsUpToDate() {
+		$file = dirname( dirname( __DIR__ ) ) . '/readme.md';
+
+		if ( ! is_file( $file ) ) {
+			$this->fail( 'No readme file present.' );
+		}
+
+		$file_contents = file_get_contents( $file );
+
+		preg_match( '|Stable tag:(.*)|i', $file_contents, $stable_tag );
+
+		$stable_version = trim( trim( $stable_tag[1], '*' ) );
+		$plugin_data    = get_plugin_data( dirname( dirname( __DIR__ ) ) . '/plugin.php' );
+
+		$this->assertSame( $stable_version, $plugin_data['Version'] );
+	}
+}
