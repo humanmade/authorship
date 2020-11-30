37d8fcd55c3f0a51deb29143f21bef14bcb92ced
John Blackbourn <john@humanmade.com>
2020-11-30 11:21:08 +0100

    Introduce template functions for getting and setting authors. Fixes #6.

--- inc/namespace.php
+++ inc/namespace.php
@@ -9,6 +9,10 @@ declare( strict_types=1 );
 
 namespace Authorship;
 
+use WP_Post;
+use WP_Term;
+use WP_User;
+
 const TAXONOMY = 'authorship';
 
 /**
@@ -19,6 +23,80 @@ function bootstrap() : void {
 	add_action( 'init', __NAMESPACE__ . '\\init_taxonomy', 99 );
 }
 
+/**
+ * Gets the user objects for the authors of the given post.
+ *
+ * @param WP_Post $post The post object.
+ * @return WP_User[] Array of user objects.
+ */
+function get_authors( WP_Post $post ) : array {
+	/**
+	 * Term objects.
+	 *
+	 * @var WP_Term[] $authors
+	 */
+	$authors = wp_get_post_terms( $post->ID, TAXONOMY );
+
+	if ( empty( $authors ) ) {
+		return [];
+	}
+
+	/**
+	 * User objects.
+	 *
+	 * @var WP_User[] $users
+	 */
+	$users = get_users( [
+		'include' => array_map( function( WP_Term $term ) : int {
+			return intval( $term->name );
+		}, $authors ),
+		'orderby' => 'ID',
+		'order'   => 'ASC',
+	] );
+
+	return $users;
+}
+
+/**
+ * Sets the authors for the given post.
+ *
+ * @param WP_Post $post    The post object.
+ * @param int[]   $authors Array of user IDs.
+ * @throws \Exception If any of the users do not exist.
+ * @return WP_User[] Array of user objects.
+ */
+function set_authors( WP_Post $post, array $authors ) : array {
+	/**
+	 * Author IDs mapped to integers.
+	 *
+	 * @var int[] $authors
+	 */
+	$authors = array_filter( array_map( 'intval', $authors ) );
+
+	/**
+	 * User objects.
+	 *
+	 * @var WP_User[] $users
+	 */
+	$users = get_users( [
+		'include' => $authors,
+		'orderby' => 'ID',
+		'order'   => 'ASC',
+	] );
+
+	if ( count( $users ) !== count( $authors ) ) {
+		throw new \Exception( __( 'One or more user IDs are not valid for this site.', 'authorship' ) );
+	}
+
+	// Author IDs must be mapped to strings before passing to `wp_set_post_terms()`.
+	$terms = wp_set_post_terms( $post->ID, array_map( 'strval', $authors ), TAXONOMY );
+
+	if ( is_wp_error( $terms ) ) {
+		throw new \Exception( $terms->get_error_message() );
+	}
+
+	return $users;
+}
 /**
  * Registers the taxonomy that creates a connection between posts and users.
  */
