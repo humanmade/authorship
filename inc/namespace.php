913139584d448ffeeb60bfabea117ba465b421c1
John Blackbourn <john@humanmade.com>
2020-11-30 12:16:57 +0100

    VS Code has a different idea of ordering than PHPCS.

--- inc/namespace.php
+++ inc/namespace.php
@@ -10,8 +10,8 @@ declare( strict_types=1 );
 namespace Authorship;
 
 use WP_Error;
-use WP_HTTP_Response;
 use WP_Http;
+use WP_HTTP_Response;
 use WP_Post;
 use WP_REST_Request;
 use WP_REST_Response;
