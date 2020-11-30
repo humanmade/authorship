2100e2ee2629f6549e4fcb963844ec04b89af9a9
John Blackbourn <john@humanmade.com>
2020-11-30 13:39:38 +0100

    First pass at a control here using the `PluginPostStatusInfo` slot.

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,24 @@
 import * as React from 'react';
+
+import { PluginPostStatusInfo } from '@wordpress/edit-post';
+
+declare const wp;
+
+const registerPlugin = wp.plugins.registerPlugin;
+
+/**
+ * Renders the author selector control.
+ *
+ * @returns {JSX.Element} An element.
+ */
+const AuthorsSelect = () => {
+	return (
+		<PluginPostStatusInfo>
+			<p>Hello, World!</p>
+		</PluginPostStatusInfo>
+	);
+};
+
+registerPlugin( 'authorship', {
+	render: AuthorsSelect,
+} );
