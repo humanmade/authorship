0ba429423ce5c426cd2c17fefe4a6a67f846bc0a
John Blackbourn <john@humanmade.com>
2020-11-30 16:06:00 +0100

    Introduce a function for fetching authors from the default `wp/v2/users` endpoint. This will be changed out later for our custom endpoint.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
+import type { WP_REST_API_User as User } from 'wp-types';
 
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
+import { addQueryArgs } from '@wordpress/url';
 
 declare const wp;
 
 const registerPlugin = wp.plugins.registerPlugin;
+
+interface Option {
+	/**
+	 * The option value.
+	 */
+	value: number;
+	/**
+	 * The option label.
+	 */
+	label: string;
+	/**
+	 * The option avatar.
+	 */
+	avatar: string;
+}
 
 /**
  * Renders the author selector control.
@@ -12,6 +29,36 @@ const registerPlugin = wp.plugins.registerPlugin;
  * @returns {JSX.Element} An element.
  */
 const AuthorsSelect = () => {
+	/**
+	 * Asynchronously loads the options for the control based on the search paramter.
+	 *
+	 * @param {string} search The search string.
+	 * @returns {Promise<Option[]>} A promise that fulfils the options.
+	 */
+	const loadOptions = ( search: string ) => {
+		const path = addQueryArgs(
+			'/wp/v2/users/',
+			{
+				search,
+				who: 'authors',
+			}
+		);
+
+		const api: Promise<User[]> = wp.apiFetch( { path } );
+
+		return api.then( users =>
+			users.map( user => {
+				const option: Option = {
+					value: user.id,
+					label: user.name,
+					avatar: user.avatar_urls[48] ?? null,
+				};
+
+				return option;
+			} )
+		);
+	};
+
 	return (
 		<PluginPostStatusInfo>
 			<p>Hello, World!</p>
