fbc60c12461a8a61f92de4aae875694b86b5a61b
John Blackbourn <john@humanmade.com>
2020-11-30 16:11:28 +0100

    We're not allowing user creation just yet, but the "Create new" option should show up even when there are matching results.

--- src/index.tsx
+++ src/index.tsx
@@ -72,6 +72,7 @@ const AuthorsSelect = () => {
 				defaultOptions={ currentAuthors }
 				isClearable={ false }
 				isMulti
+				isValidNewOption={ value => value.length >= 2 }
 				loadOptions={ loadOptions }
 			/>
 		</PluginPostStatusInfo>
