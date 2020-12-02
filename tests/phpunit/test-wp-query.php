b2774b3355586a7ccf3a06e5c545f28d2aa3b42a
John Blackbourn <john@humanmade.com>
2020-12-02 18:33:44 +0100

    Add some tests for the `WP_Query` filtering.

---
+++ tests/phpunit/test-wp-query.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * WP_Query tests.
+ *
+ * @package authorship
+ */
+
+declare( strict_types=1 );
+
+namespace Authorship\Tests;
+
+use const Authorship\POSTS_PARAM;
+
+use WP_Query;
+
+class TestWPQuery extends TestCase {
+	public function testQueryForAuthorNameReturnsPostsAttributedToAuthor() {
+		$factory = self::factory()->post;
+
+		// Attributed to Editor, owned by Admin.
+		$yes1 = $factory->create_and_get( [
+			'post_author' => self::$users['admin']->ID,
+			POSTS_PARAM   => [
+				self::$users['editor']->ID,
+			],
+		] );
+
+		// Attributed to Editor, owned by nobody.
+		$yes2 = $factory->create_and_get( [
+			POSTS_PARAM => [
+				self::$users['editor']->ID,
+			],
+		] );
+
+		// Attributed to and owned by Author.
+		$no1 = $factory->create_and_get( [
+			'post_author' => self::$users['author']->ID,
+		] );
+
+		// Attributed to Admin, owned by Editor.
+		$no2 = $factory->create_and_get( [
+			'post_author' => self::$users['editor']->ID,
+			POSTS_PARAM   => [
+				self::$users['admin']->ID,
+			],
+		] );
+
+		$args = [
+			'author_name' => self::$users['editor']->user_nicename,
+			'fields'      => 'ids',
+			'orderby'     => 'ID',
+			'order'       => 'ASC',
+		];
+
+		$query = new WP_Query();
+		$posts = $query->query( $args );
+
+		$this->assertCount( 2, $posts );
+		$this->assertSame( [ $yes1->ID, $yes2->ID ], $posts );
+	}
+
+	public function testQueriedObjectIsRetained() {
+		$factory = self::factory()->post;
+
+		// Attributed to Editor, owned by Admin.
+		$factory->create_and_get( [
+			'post_author' => self::$users['admin']->ID,
+			POSTS_PARAM   => [
+				self::$users['editor']->ID,
+			],
+		] );
+
+		$args = [
+			'post_type'   => 'post',
+			'author_name' => self::$users['editor']->user_nicename,
+			'fields'      => 'ids',
+			'post_status' => 'publish',
+		];
+
+		$query = new WP_Query( $args );
+
+		$this->assertSame( self::$users['editor']->ID, $query->get_queried_object_id() );
+		$this->assertInstanceOf( 'WP_User', $query->get_queried_object() );
+	}
+}
