e02bfebfef4b999cba5994305dcab1499dfd68e2
John Blackbourn <john@humanmade.com>
2020-12-10 16:48:55 +0100

    Test all non-admin roles.

--- tests/phpunit/test-capabilities.php
+++ tests/phpunit/test-capabilities.php
@@ -23,10 +23,10 @@ class TestCapabilities extends TestCase {
 	 * @dataProvider dataRolesAndCaps
 	 *
 	 * @param string $role Role name
+	 * @param mixed[] $caps Caps
 	 */
-	public function testUserCanManagePostTheyAreAttributedTo( string $role ) : void {
+	public function testUserCanManagePostTheyAreAttributedTo( string $role, array $caps ) : void {
 		$factory = self::factory()->post;
-
 		$user_id = self::$users[ $role ]->ID;
 
 		// Attributed to user, owned by Admin.
@@ -58,22 +58,22 @@ class TestCapabilities extends TestCase {
 		] );
 
 		// Draft post:
-		$this->assertTrue( user_can( $user_id, 'edit_post', $draft_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'publish_post', $draft_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'read_post', $draft_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'delete_post', $draft_post->ID ) );
+		$this->assertSame( $caps['draft']['edit_post'], user_can( $user_id, 'edit_post', $draft_post->ID ) );
+		$this->assertSame( $caps['draft']['publish_post'], user_can( $user_id, 'publish_post', $draft_post->ID ) );
+		$this->assertSame( $caps['draft']['read_post'], user_can( $user_id, 'read_post', $draft_post->ID ) );
+		$this->assertSame( $caps['draft']['delete_post'], user_can( $user_id, 'delete_post', $draft_post->ID ) );
 
 		// Published post:
-		$this->assertTrue( user_can( $user_id, 'edit_post', $published_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'publish_post', $published_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'read_post', $published_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'delete_post', $published_post->ID ) );
+		$this->assertSame( $caps['publish']['edit_post'], user_can( $user_id, 'edit_post', $published_post->ID ) );
+		$this->assertSame( $caps['publish']['publish_post'], user_can( $user_id, 'publish_post', $published_post->ID ) );
+		$this->assertSame( $caps['publish']['read_post'], user_can( $user_id, 'read_post', $published_post->ID ) );
+		$this->assertSame( $caps['publish']['delete_post'], user_can( $user_id, 'delete_post', $published_post->ID ) );
 
 		// Scheduled post:
-		$this->assertTrue( user_can( $user_id, 'edit_post', $scheduled_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'publish_post', $scheduled_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'read_post', $scheduled_post->ID ) );
-		$this->assertTrue( user_can( $user_id, 'delete_post', $scheduled_post->ID ) );
+		$this->assertSame( $caps['future']['edit_post'], user_can( $user_id, 'edit_post', $scheduled_post->ID ) );
+		$this->assertSame( $caps['future']['publish_post'], user_can( $user_id, 'publish_post', $scheduled_post->ID ) );
+		$this->assertSame( $caps['future']['read_post'], user_can( $user_id, 'read_post', $scheduled_post->ID ) );
+		$this->assertSame( $caps['future']['delete_post'], user_can( $user_id, 'delete_post', $scheduled_post->ID ) );
 	}
 
 	/**
@@ -82,7 +82,96 @@ class TestCapabilities extends TestCase {
 	public function dataRolesAndCaps() : array {
 		return [
 			[
+				'editor',
+				[
+					'draft' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+					'publish' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+					'future' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+				],
+			],
+			[
 				'author',
+				[
+					'draft' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+					'publish' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+					'future' => [
+						'edit_post'    => true,
+						'publish_post' => true,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+				],
+			],
+			[
+				'contributor',
+				[
+					'draft' => [
+						'edit_post'    => true,
+						'publish_post' => false,
+						'read_post'    => true,
+						'delete_post'  => true,
+					],
+					'publish' => [
+						'edit_post'    => false,
+						'publish_post' => false,
+						'read_post'    => true,
+						'delete_post'  => false,
+					],
+					'future' => [
+						'edit_post'    => false,
+						'publish_post' => false,
+						'read_post'    => false, // @TODO Can a Contributor not read their own scheduled post?
+						'delete_post'  => false,
+					],
+				],
+			],
+			[
+				'subscriber',
+				[
+					'draft' => [
+						'edit_post'    => false,
+						'publish_post' => false,
+						'read_post'    => false,
+						'delete_post'  => false,
+					],
+					'publish' => [
+						'edit_post'    => false,
+						'publish_post' => false,
+						'read_post'    => true,
+						'delete_post'  => false,
+					],
+					'future' => [
+						'edit_post'    => false,
+						'publish_post' => false,
+						'read_post'    => false,
+						'delete_post'  => false,
+					],
+				],
 			],
 		];
 	}
