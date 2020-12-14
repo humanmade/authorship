d3f61e1c285bbe9eb6a1390cab118498e59d7abe
John Blackbourn <john@humanmade.com>
2020-12-14 12:17:24 +0100

    More tests.

--- tests/phpunit/test-rest-api-user-endpoint.php
+++ tests/phpunit/test-rest-api-user-endpoint.php
@@ -20,11 +20,119 @@ class TestRESTAPIUserEndpoint extends RESTAPITestCase {
 		wp_set_current_user( self::$users['admin']->ID );
 
 		$request = new WP_REST_Request( 'GET', self::$route );
-		$request->set_param( 'search', 'editor' );
+		$request->set_param( 'search', 'testing' );
 		$request->set_param( 'roles', 'editor' );
 
 		$response = self::rest_do_request( $request );
 
 		$this->assertSame( WP_Http::FORBIDDEN, $response->get_status() );
+	}
+
+	public function testEndpointRequiresAuthentication() : void {
+		$request = new WP_REST_Request( 'GET', self::$route );
+		$request->set_param( 'search', 'testing' );
+
+		$response = self::rest_do_request( $request );
+
+		$this->assertSame( WP_Http::UNAUTHORIZED, $response->get_status() );
+	}
+
+	public function testSearchParameterIsRequiredWhenListingUsers() : void {
+		wp_set_current_user( self::$users['admin']->ID );
+
+		$request = new WP_REST_Request( 'GET', self::$route );
+		$response = self::rest_do_request( $request );
+
+		$this->assertTrue( $response->is_error() );
+
+		/** @var \WP_Error */
+		$error = $response->as_error();
+		$data = $error->get_error_data();
+
+		$this->assertSame( WP_Http::BAD_REQUEST, $response->get_status() );
+		$this->assertArrayHasKey( 'params', $data );
+		$this->assertContains( 'search', $data['params'] );
+	}
+
+	/**
+	 * @dataProvider dataAllowedOrderby
+	 *
+	 * @param string $orderby
+	 */
+	public function testAllowedOrderByParameters( string $orderby ) : void {
+		wp_set_current_user( self::$users['admin']->ID );
+
+		$request = new WP_REST_Request( 'GET', self::$route );
+		$request->set_param( 'search', 'testing' );
+		$request->set_param( 'orderby', $orderby );
+
+		$response = self::rest_do_request( $request );
+
+		$this->assertSame( WP_Http::OK, $response->get_status() );
+	}
+
+	/**
+	 * @dataProvider dataDisallowedOrderby
+	 *
+	 * @param string $orderby
+	 */
+	public function testDisallowedOrderByParameters( string $orderby ) : void {
+		wp_set_current_user( self::$users['admin']->ID );
+
+		$request = new WP_REST_Request( 'GET', self::$route );
+		$request->set_param( 'search', 'testing' );
+		$request->set_param( 'orderby', $orderby );
+
+		$response = self::rest_do_request( $request );
+
+		$this->assertTrue( $response->is_error() );
+
+		/** @var \WP_Error */
+		$error = $response->as_error();
+		$data = $error->get_error_data();
+
+		$this->assertSame( WP_Http::BAD_REQUEST, $response->get_status() );
+		$this->assertArrayHasKey( 'params', $data );
+		$this->assertArrayHasKey( 'orderby', $data['params'] );
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	public function dataAllowedOrderby() : array {
+		return [
+			[
+				'id',
+			],
+			[
+				'name',
+			],
+		];
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	public function dataDisallowedOrderby() : array {
+		return [
+			[
+				'include',
+			],
+			[
+				'registered_date',
+			],
+			[
+				'slug',
+			],
+			[
+				'include_slugs',
+			],
+			[
+				'email',
+			],
+			[
+				'url',
+			],
+		];
 	}
 }
