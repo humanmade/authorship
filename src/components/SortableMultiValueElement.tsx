de4890d2d69fea26542e790a4e62112155f367f8
tfrommen <info@tfrommen.de>
2021-01-06 23:29:50 +0100

    Add prop type declaration

--- src/components/SortableMultiValueElement.tsx
+++ src/components/SortableMultiValueElement.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { components } from 'react-select';
+import { components, MultiValueProps } from 'react-select';
 import { SortableElement } from 'react-sortable-hoc';
 
 const { MultiValue } = components;
 
-const MultiValueElement = props => {
+const MultiValueElement = ( props : MultiValueProps<Option> ) => {
 	// This prevents the menu from being opened/closed when the user clicks
 	// on a value to begin dragging it.
 	const innerProps = {
