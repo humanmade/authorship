91abf6b401181092e7da03d9d302f0bfa815d914
tfrommen <info@tfrommen.de>
2021-01-07 11:46:26 +0100

    Move plugin data into dedicated file

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,7 @@
-import * as React from 'react';
-
-import { compose } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
-import { PluginPostStatusInfo } from '@wordpress/edit-post';
-import { get } from 'lodash';
-
-import AuthorsSelect from './components/authors-select';
+import { name, settings } from './plugin';
 
 declare const wp: any;
 
 const registerPlugin = wp.plugins.registerPlugin;
 
-const Select = compose( [
-	withDispatch( dispatch => ( {
-		onUpdate( value: number[] ) {
-			dispatch( 'core/editor' ).editPost( {
-				authorship: value,
-			} );
-		},
-		onError( message: string ) {
-			dispatch( 'core/notices' ).createErrorNotice( message );
-		},
-	} ) ),
-	withSelect( select => {
-		const post = select( 'core/editor' ).getCurrentPost();
-		return {
-			hasAssignAuthorAction: get(
-				post,
-				[ '_links', 'authorship:action-assign-authorship' ],
-				false
-			),
-		};
-	} ),
-] )( AuthorsSelect );
-
-const render = () => (
-	<PluginPostStatusInfo>
-		<Select/>
-	</PluginPostStatusInfo>
-);
-
-registerPlugin( 'authorship', { render } );
+registerPlugin( name, settings );
---
+++ src/plugin.tsx
@@ -0,0 +1,43 @@
+import { get } from 'lodash';
+import * as React from 'react';
+
+import { compose } from '@wordpress/compose';
+import { withDispatch, withSelect } from '@wordpress/data';
+import { PluginPostStatusInfo } from '@wordpress/edit-post';
+
+import AuthorsSelect from './components/authors-select';
+
+const Select = compose( [
+	withDispatch( dispatch => ( {
+		onUpdate( value: number[] ) {
+			dispatch( 'core/editor' ).editPost( {
+				authorship: value,
+			} );
+		},
+		onError( message: string ) {
+			dispatch( 'core/notices' ).createErrorNotice( message );
+		},
+	} ) ),
+	withSelect( select => {
+		const post = select( 'core/editor' ).getCurrentPost();
+		return {
+			hasAssignAuthorAction: get(
+				post,
+				[ '_links', 'authorship:action-assign-authorship' ],
+				false
+			),
+		};
+	} ),
+] )( AuthorsSelect );
+
+const render = () => (
+	<PluginPostStatusInfo>
+		<Select/>
+	</PluginPostStatusInfo>
+);
+
+export const name = 'authorship';
+
+export const settings = {
+	render,
+};
