6c5b871fd852ca81fe26b0bebefde58083fdf9ad
John Blackbourn <john@humanmade.com>
2021-01-26 17:22:19 +0100

    Add a test for the `get_author_ids()` template function.

--- tests/phpunit/test-template.php
+++ tests/phpunit/test-template.php
@@ -11,6 +11,7 @@ namespace Authorship\Tests;
 
 use const Authorship\POSTS_PARAM;
 
+use function Authorship\get_author_ids;
 use function Authorship\user_is_author;
 
 use WP_User;
@@ -51,6 +52,42 @@ class TestTemplate extends TestCase {
 		$post = $factory->create_and_get( $args );
 
 		$this->assertSame( $expected, user_is_author( $user, $post ) );
+	}
+
+	/**
+	 * @dataProvider dataAuthorsYes
+	 */
+	public function testAuthorIDs( bool $is_author, callable $user, ?callable $post_author, callable ...$authorship ) : void {
+		$factory = self::factory()->post;
+
+		if ( is_callable( $post_author ) ) {
+			/** @var int */
+			$post_author = call_user_func( $post_author )->ID;
+		}
+
+		/** @var \WP_User[] */
+		$post_authorship = array_map( 'call_user_func', $authorship );
+
+		/** @var int[] */
+		$post_authorship = array_map( function( WP_User $user ) : int {
+			return $user->ID;
+		}, $post_authorship );
+
+		$args = [];
+		$expected = [];
+
+		if ( $post_author ) {
+			$args['post_author'] = $post_author;
+			$expected = [ $post_author ];
+		}
+		if ( $post_authorship ) {
+			$args[ POSTS_PARAM ] = $post_authorship;
+			$expected = $post_authorship;
+		}
+
+		$post = $factory->create_and_get( $args );
+
+		$this->assertSame( $expected, get_author_ids( $post ) );
 	}
 
 	/**
