a5e7356a9271a550df09863163ed86d9fb0aa49f
Tom J Nowell <contact@tomjn.com>
2022-11-15 20:55:56 +0000

    refactors attribute_post_type handling

--- inc/namespace.php
+++ inc/namespace.php
@@ -245,26 +245,33 @@ function filter_user_has_cap( array $user_caps, array $required_caps, array $arg
 			break;
 
 		case 'attribute_post_type':
-			if ( empty( $args[2] ) ) {
-				$user_caps[ $cap ] = false;
+			// if the user already has the cap then, defer to the existing value
+			if ( array_key_exists( $cap, $user_caps ) ) {
 				break;
 			}
 
+			// fetch the post type
 			$post_type_object = get_post_type_object( $args[2] );
 
+			// if it's for an undefined post type, then they do not have `attribute_post_type`
 			if ( ! $post_type_object ) {
 				$user_caps[ $cap ] = false;
 				break;
 			}
 
-			if ( array_key_exists( $cap, $user_caps ) ) {
-				break;
-			}
 
+			// fallback to post type edit others post
 			/** @var stdClass */
 			$post_type_caps = $post_type_object->cap;
 
-			$user_caps[ $cap ] = user_can( $user->ID, $post_type_caps->edit_others_posts );
+			// if we are not checking a specific post, then check if they can edit other posts of this type
+			if ( empty( $args[2] ) ) {
+				$user_caps[ $cap ] = user_can( $user->ID, $post_type_caps->edit_others_posts );
+				break;
+			}
+
+			// otherwise we _are_ checking a specific post, so perform the check but with the post ID
+			$user_caps[ $cap ] = user_can( $user->ID, $post_type_caps->edit_others_posts, $args[2] );
 			break;
 
 	}//end switch
